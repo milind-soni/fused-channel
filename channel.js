e2ca118d464e20b592f9350077546e1b4fc4d28a
Milind Soni <46266943+milind-soni@users.noreply.github.com>
2025-10-12 01:56:44 +0530

    Update channel.js

--- channel.js
+++ channel.js
@@ -1,35 +1,47 @@
 (function (global) {
-  function fusedChannel(name = "fused-default") {
-    const t = ("BroadcastChannel" in window)
-      ? new BroadcastChannel(name)
-      : new EventTarget();
-
-    function publish(type, payload = {}, origin = "udf") {
-      const msg = { type, payload, origin, channel: name, ts: Date.now() };
-      if (t instanceof BroadcastChannel)
-        t.postMessage(msg);
-      else
-        t.dispatchEvent(new CustomEvent(type, { detail: msg }));
-    }
-
-    function on(type, handler) {
-      if (t instanceof BroadcastChannel) {
-        t.addEventListener("message", e => {
-          if (e.data?.type === type) handler(e.data);
-        });
-      } else {
-        t.addEventListener(type, e => handler(e.detail));
+  if (!global.fusedChannel) {
+    global.fusedChannel = function(name = "fused-default") {
+      const t = ("BroadcastChannel" in window)
+        ? new BroadcastChannel(name)
+        : new EventTarget();
+      function publish(type, payload = {}, origin = "udf") {
+        const msg = { type, payload, origin, channel: name, ts: Date.now() };
+        if (t instanceof BroadcastChannel) t.postMessage(msg);
+        else t.dispatchEvent(new CustomEvent(type, { detail: msg }));
       }
-    }
-
-    function close() {
-      if (t.close) t.close();
-    }
-
-    return { publish, on, close };
+      function on(type, handler) {
+        if (t instanceof BroadcastChannel) {
+          t.addEventListener("message", e => {
+            if (e.data?.type === type) handler(e.data);
+          });
+        } else {
+          t.addEventListener(type, e => handler(e.detail));
+        }
+      }
+      function close() { if (t.close) t.close(); }
+      return { publish, on, close };
+    };
   }
 
-  // expose globally and as module export if available
-  global.fusedChannel = fusedChannel;
-  if (typeof module !== "undefined") module.exports = fusedChannel;
+
+  global.enableBoundsMessaging = function(map, channel, sender) {
+    const ch = fusedChannel(channel);
+    function sendBounds() {
+      const b = map.getBounds();
+      ch.publish('bounds', {
+        bounds: [b.getWest(), b.getSouth(), b.getEast(), b.getNorth()],
+        zoom: map.getZoom()
+      }, sender);
+    }
+    if (map.loaded && map.loaded()) {
+      sendBounds();
+      map.on('move', sendBounds);   // 0-debounce
+    } else {
+      map.once('load', () => {
+        sendBounds();
+        map.on('move', sendBounds);
+      });
+    }
+    window.addEventListener('beforeunload', () => ch.close && ch.close());
+  };
 })(this);
