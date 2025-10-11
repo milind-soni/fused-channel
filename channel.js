7a5f98aa67d2ff2d5efe21fa247e325fa8f40b05
Milind Soni <46266943+milind-soni@users.noreply.github.com>
2025-10-12 02:37:22 +0530

    Update channel.js

--- channel.js
+++ channel.js
@@ -1,9 +1,7 @@
 (function (global) {
   if (!global.fusedChannel) {
     global.fusedChannel = function(name = "fused-default") {
-      const t = ("BroadcastChannel" in window)
-        ? new BroadcastChannel(name)
-        : new EventTarget();
+      const t = ("BroadcastChannel" in window) ? new BroadcastChannel(name) : new EventTarget();
       function publish(type, payload = {}, origin = "udf") {
         const msg = { type, payload, origin, channel: name, ts: Date.now() };
         if (t instanceof BroadcastChannel) t.postMessage(msg);
@@ -11,9 +9,7 @@
       }
       function on(type, handler) {
         if (t instanceof BroadcastChannel) {
-          t.addEventListener("message", e => {
-            if (e.data?.type === type) handler(e.data);
-          });
+          t.addEventListener("message", e => { if (e.data?.type === type) handler(e.data); });
         } else {
           t.addEventListener(type, e => handler(e.detail));
         }
@@ -23,25 +19,35 @@
     };
   }
 
-
-  global.enableBoundsMessaging = function(map, channel, sender) {
+  global.enableMessaging = function({ source, channel, sender, type, on, off, getPayload }) {
     const ch = fusedChannel(channel);
-    function sendBounds() {
-      const b = map.getBounds();
-      ch.publish('bounds', {
-        bounds: [b.getWest(), b.getSouth(), b.getEast(), b.getNorth()],
-        zoom: map.getZoom()
-      }, sender);
-    }
-    if (map.loaded && map.loaded()) {
-      sendBounds();
-      map.on('move', sendBounds);   // 0-debounce
-    } else {
-      map.once('load', () => {
-        sendBounds();
-        map.on('move', sendBounds);
-      });
-    }
+    const handler = (...args) => ch.publish(type, getPayload(...args), sender);
+    on(source, handler);
     window.addEventListener('beforeunload', () => ch.close && ch.close());
+    return () => { try { off && off(source, handler); } catch (_) {} };
+  };
+
+  global.enableBoundsMessaging = function(map, channel, sender, event = 'move') {
+    const getPayload = () => {
+      const b = map.getBounds();
+      return { bounds: [b.getWest(), b.getSouth(), b.getEast(), b.getNorth()], zoom: map.getZoom() };
+    };
+    const on  = (m, h) => { (m.loaded && m.loaded()) ? h() : m.once('load', h); m.on(event, h); };
+    const off = (m, h) => m.off(event, h);
+    return global.enableMessaging({ source: map, channel, sender, type: 'bounds', on, off, getPayload });
+  };
+
+  global.enableBrushMessaging = function(view, channel, sender, signal = 'brush') {
+    const on  = (v, h) => v.addSignalListener(signal, h);
+    const off = (v, h) => v.removeSignalListener(signal, h);
+    const getPayload = (name, value) => ({ signal: name, extent: value });
+    return global.enableMessaging({ source: view, channel, sender, type: 'brush', on, off, getPayload });
+  };
+
+  global.enableButtonMessaging = function(el, channel, sender, event = 'click', basePayload = {}) {
+    const on  = (e, h) => e.addEventListener(event, h);
+    const off = (e, h) => e.removeEventListener(event, h);
+    const getPayload = (ev) => ({ id: el.id || null, event, ...basePayload });
+    return global.enableMessaging({ source: el, channel, sender, type: 'button', on, off, getPayload });
   };
 })(this);
